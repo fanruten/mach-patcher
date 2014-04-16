166263357c49ac09434af57901facf50b27329cf
Ruslan Gumenniy <ruslan.gumenniy@e-legion.com>
2014-04-17 02:51:15 +0400

    Add inject in fat binary.

--- mach-patcher/main.cpp
+++ mach-patcher/main.cpp
@@ -8,7 +8,52 @@
 
 #include <iostream>
 #include <libgen.h>
+#include <stdint.h>
+#include <sys/_endian.h>
+
 #include "loader.h"
+#include "fat.h"
+
+void patchMachHeader(void *mem, uint32_t offset, char *injectedLibName)
+{
+    mach_header *header = (mach_header *)((uint8_t *)mem + offset);
+    int commandsCount = header->ncmds;
+    printf("Commands count: %d\n", commandsCount);
+    
+    dylib_command *lastDylibCommand = NULL;
+    load_command *command = (load_command *)((uint8_t *)header + sizeof(mach_header));
+    for (int i = 0; i < commandsCount; i++) {
+        if (command->cmd == LC_LOAD_DYLIB) {
+            dylib_command *typedCommand = (dylib_command *)command;
+            lastDylibCommand = typedCommand;
+            printf("LC_LOAD_DYLIB: %s\n", (char *)((uint8_t *)typedCommand + typedCommand->dylib.name.offset));
+        }
+        command = (load_command *)((uint8_t *)command + command->cmdsize);
+    }
+    
+    if (lastDylibCommand) {
+        int libNameSize = (int)strlen(injectedLibName) + 1;
+        int padding = 4 - libNameSize % 4;
+        
+        dylib_command injectedCommand;
+        injectedCommand.cmd = LC_LOAD_DYLIB;
+        injectedCommand.cmdsize = 24 + libNameSize + padding;
+        injectedCommand.dylib.current_version = 0x10000;
+        injectedCommand.dylib.compatibility_version = 0x10000;
+        injectedCommand.dylib.timestamp = 0;
+        injectedCommand.dylib.name.offset = 24;
+        
+        uint8_t *newCommandPos = (uint8_t *)lastDylibCommand + lastDylibCommand->cmdsize;
+        long bytesForMoving = header->sizeofcmds + sizeof(mach_header) - (int)((uint8_t *)newCommandPos - (uint8_t *)header);
+        
+        memmove((uint8_t *)newCommandPos + injectedCommand.cmdsize, newCommandPos, bytesForMoving);
+        memcpy(newCommandPos, &injectedCommand, sizeof(injectedCommand));
+        memcpy((uint8_t *)newCommandPos + sizeof(injectedCommand), injectedLibName, libNameSize);
+        
+        header->ncmds += 1;
+        header->sizeofcmds += injectedCommand.cmdsize;
+    }
+}
 
 int main(int argc, const char * argv[])
 {
@@ -19,83 +64,43 @@ int main(int argc, const char * argv[])
         return 1;
     }
         
-    char *fileName = (char *)argv[1];
-    char *injectLib = (char *)argv[2];
+    char *binaryFileName = (char *)argv[1];
+    char *injectedLibName = (char *)argv[2];
     
-    FILE *fileToken = fopen(fileName, "r+b");
+    FILE *fileToken = fopen(binaryFileName, "r+b");
     if (!fileToken) {
         printf("File open error: %d \n", errno);
         return 1;
     }
     
-    mach_header header;
-    fread(&header, sizeof(header), 1, fileToken);
-    long lastDylibPos = 0;
-    long lastDylibSize = 0;
+    fseek(fileToken, 0, SEEK_END);
+    long fileSize = ftell(fileToken);
+    fseek(fileToken, 0, SEEK_SET);
+    void *buf = malloc(fileSize);
+    fread(buf, 1, fileSize, fileToken);
+    fclose(fileToken);
     
-    for (int i = 0; i < header.ncmds; i++) {
-        long commandPos = ftell(fileToken);
-        
-        load_command command;
-        fread(&command, sizeof(command), 1, fileToken);
+    if (*(uint32_t *)buf == FAT_CIGAM) {
+        fat_header *fatHeader = (fat_header *)buf;
+        int archCount = ntohl(fatHeader->nfat_arch);
+        printf("Archs count: %d\n", archCount);
 
-#ifdef DEBUG_INFO
-        printf("cmd: %d size: %d\n", command.cmd, command.cmdsize);
-#endif
-        if (command.cmd == LC_LOAD_DYLIB) {
-            lastDylibPos = commandPos;
-            lastDylibSize = command.cmdsize;
-
-#ifdef DEBUG_INFO
-            dylib_command typedCommand;
-            fseek(fileToken, commandPos, SEEK_SET);
-            fread(&typedCommand, sizeof(dylib), 1, fileToken);
+        for (int i = 0; i < archCount; i++) {
+            fat_arch *fatArch = (fat_arch *)((uint8_t *)fatHeader + sizeof(fat_header) + sizeof(fat_arch) * i);
+            printf("CPU type: %d\nCPU subtype: %d\nOffset: %d\nSize: %d\nAlign: %d\n",
+                   ntohl(fatArch->cputype), ntohl(fatArch->cputype), ntohl(fatArch->offset), ntohl(fatArch->size), ntohl(fatArch->align));
             
-            int nameOfset = typedCommand.dylib.name.offset;
-            int nameSize = typedCommand.cmdsize - nameOfset;
-            
-            fseek(fileToken, commandPos + nameOfset, SEEK_SET);
-            char *name = (char *)calloc(nameSize, sizeof(char));
-            fread(name, nameSize, 1, fileToken);
-            printf("lib: %s\n", name);
-            free(name);
-#endif
+            patchMachHeader(buf, ntohl(fatArch->offset), injectedLibName);
         }
-        
-        fseek(fileToken, commandPos + command.cmdsize, SEEK_SET);
+    } else if (*(uint32_t *)buf == MH_MAGIC) {
+        patchMachHeader(buf, 0, injectedLibName);
     }
     
-    int libNameSize = (int)strlen(injectLib) + 1;
-    int padding = 4 - libNameSize % 4;
-    
-    dylib_command injectedCommand;
-    injectedCommand.cmd = LC_LOAD_DYLIB;
-    injectedCommand.cmdsize = 24 + libNameSize + padding;
-    injectedCommand.dylib.current_version = 0x10000;
-    injectedCommand.dylib.compatibility_version = 0x10000;
-    injectedCommand.dylib.timestamp = 0;
-    injectedCommand.dylib.name.offset = 24;
-    
-    long newCommandPos = lastDylibPos + lastDylibSize;
-    long bytesForMoving = header.sizeofcmds + sizeof(header) - newCommandPos;
-    void *tmpBuf = calloc(bytesForMoving, 1);
-    fseek(fileToken, newCommandPos, SEEK_SET);
-    fread(tmpBuf, bytesForMoving, 1, fileToken);
-    
-    fseek(fileToken, newCommandPos, SEEK_SET);
-    fwrite(&injectedCommand, sizeof(injectedCommand), 1, fileToken);
-    fwrite(injectLib, libNameSize, 1, fileToken);
-    
-    fseek(fileToken, newCommandPos + injectedCommand.cmdsize, SEEK_SET);
-    fwrite(tmpBuf, bytesForMoving, 1, fileToken);
-    free(tmpBuf);
-    
-    header.ncmds += 1;
-    header.sizeofcmds += injectedCommand.cmdsize;
-    fseek(fileToken, 0, SEEK_SET);
-    fwrite(&header, sizeof(header), 1, fileToken);
-    
+    fileToken = fopen(binaryFileName, "wb");
+    fwrite(buf, 1, fileSize, fileToken);
     fclose(fileToken);
+    
+    free(buf);
     
     return 0;
 }
